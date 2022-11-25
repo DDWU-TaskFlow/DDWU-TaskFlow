992d05962958290a25b1c4049f4f96b4a3144453
Songhee Lee <clasonghee@gmail.com>
2022-11-25 23:18:04 +0900

    Feat: dto 클래스 추가

--- src/main/java/controller/project/ListProjectController.java
+++ src/main/java/controller/project/ListProjectController.java
@@ -1,16 +1,26 @@
 package controller.project;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import model.Community;
+import model.service.UserManager;
 
 public class ListProjectController implements Controller {
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		return null;
+		
+    	UserManager manager = UserManager.getInstance();
+		List<Community> commList = manager.findCommunityList();
+		
+		// commList 객체를 request에 저장하여 커뮤니티 리스트 화면으로 이동(forwarding)
+		request.setAttribute("commList", commList);				
+		return "/community/list.jsp";       
 	}
 
 }
