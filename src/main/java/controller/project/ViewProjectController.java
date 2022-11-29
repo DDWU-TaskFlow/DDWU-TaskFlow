fcccf09e5b20d966d8bd89a673783ea03999e8fb
JaeHyeonSim <peanutandnuts@gmail.com>
2022-11-29 12:47:07 +0900

    Feat: project test

--- src/main/java/controller/project/ViewProjectController.java
+++ src/main/java/controller/project/ViewProjectController.java
@@ -4,13 +4,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import controller.Controller;
+import model.Project;
+import model.service.ProjectManager;
 
 public class ViewProjectController implements Controller{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+    	Project project = null;
+    	ProjectManager manager = ProjectManager.getInstance();
+		int projectId = Integer.parseInt(request.getParameter("projectId"));
+//		project = manager.getProject(projectId);		// 프로젝트 정보 검색	
+		
+		request.setAttribute("project", projectId);	// 프로젝트 정보 저장, 일단 ID 저장			
+		return "/project/projectView.jsp";				// 프로젝트 보기 화면으로 이동
 	}
 
 }
