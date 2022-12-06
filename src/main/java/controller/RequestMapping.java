b506d8082811f693a87e17b5ae40e0698d65576d
JaeHyeonSim <peanutandnuts@gmail.com>
2022-12-06 13:31:52 +0900

    Feat: updateProject.jsp - data 불러오기 작업

--- src/main/java/controller/RequestMapping.java
+++ src/main/java/controller/RequestMapping.java
@@ -17,6 +17,7 @@ import controller.project.ListProjectController;
 import controller.project.OutProjectController;
 import controller.project.UpdateProjectController;
 import controller.project.ViewProjectController;
+import controller.task.UpdateTaskController;
 
 public class RequestMapping {
     private static final Logger logger = LoggerFactory.getLogger(DispatcherServlet.class);
@@ -38,13 +39,16 @@ public class RequestMapping {
         mappings.put("/member/entrance", new EnterMemberController());
         mappings.put("/member/mypage", new MyPageController());
         
-        // projeDct
+        // project
         mappings.put("/project/create", new CreateProjectController());	// get, post
         mappings.put("/project/update", new UpdateProjectController());
         mappings.put("/project/delete", new DeleteProjectController());
         mappings.put("/project/setting/out", new OutProjectController());
         mappings.put("/project/list", new ListProjectController());
         mappings.put("/project/view", new ViewProjectController());
+        
+        // task
+        mappings.put("/task/update", new UpdateTaskController()); // get, post
         
         logger.info("Initialized Request Mapping!");
     }
