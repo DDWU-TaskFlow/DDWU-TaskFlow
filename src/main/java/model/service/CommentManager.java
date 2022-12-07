a597b4830d37993e935f92d1e90a50996b37f1f4
JaeHyeonSim <peanutandnuts@gmail.com>
2022-12-08 02:44:14 +0900

    Feat: comment mybatis 관련 수정

--- src/main/java/model/service/CommentManager.java
+++ src/main/java/model/service/CommentManager.java
@@ -5,14 +5,17 @@ import java.util.List;
 
 import model.Comment;
 import model.dao.mybatis.CommentDao;
+import model.dao.mybatis.CommentSessionDao;
 
 public class CommentManager {
 	private static CommentManager commentManager = new CommentManager();
-	private CommentDao commentDao;
+//	private CommentDao commentDao;
+	private CommentSessionDao commentSessionDao;
 	
 	private CommentManager() {
 		try {
-			commentDao = new CommentDao();
+//			commentDao = new CommentDao();
+			commentSessionDao = new CommentSessionDao();
 			System.out.println("commentDAO 생성 완료");
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -25,13 +28,26 @@ public class CommentManager {
 	
 	public List<Comment> findCommentByTaskId(int taskId) {
 		List<Comment> commentList = new ArrayList<Comment>();
-		commentList = commentDao.findCommentByTaskId(taskId);
+		commentList = commentSessionDao.findCommentByTaskId(taskId);
 		return commentList;
 	}
 	
 	public String findMemberNameByCommentId(int commentId) {
-		String memberName = commentDao.findMemberNameByCommentId(commentId);
+		String memberName = commentSessionDao.findMemberNameByCommentId(commentId);
 		return memberName;
 	}
+	
+	public int insertComment(Comment comment) {
+		return commentSessionDao.insertComment(comment);
+	}
+	
+//	public int UpdateComment(Comment comment) {
+//		return commentDao.updateComment(comment);
+//	}
+	
+	public int deleteComment(int commentId) {
+		return commentSessionDao.deleteComment(commentId);
+	}
+	
 	
 }
