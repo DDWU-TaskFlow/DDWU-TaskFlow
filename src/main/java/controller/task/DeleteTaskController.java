cfa75ad00b90dc2690cdfe78d7cb2c48d49b5a34
JaeHyeonSim <peanutandnuts@gmail.com>
2022-12-07 20:50:54 +0900

    Feat: date 타입 수정

--- src/main/java/controller/task/DeleteTaskController.java
+++ src/main/java/controller/task/DeleteTaskController.java
@@ -11,8 +11,6 @@ public class DeleteTaskController implements Controller{
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {		
 
-		System.out.println("task delete Controller");
-		System.out.println("deletedeletedeletedeletedeletedeletedeletedeletedeletedeletedeletedeletedeletedeletedeletedelete");
 		TaskManager tManager = TaskManager.getInstance();
 		int taskId = Integer.parseInt(request.getParameter("taskId"));
 		System.out.println("삭제할 태스크" + taskId);
