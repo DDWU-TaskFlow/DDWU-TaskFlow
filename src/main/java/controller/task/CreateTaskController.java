a3bbebf82570d5b932e51bc18f19f0aa4e92fa4a
HannaSeo <shn2281@naver.com>
2022-12-09 01:46:01 +0900

    feat: 태스크 생성 기능 구현

--- src/main/java/controller/task/CreateTaskController.java
+++ src/main/java/controller/task/CreateTaskController.java
@@ -1,18 +1,24 @@
 package controller.task;
 
+import java.sql.Date;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import controller.Controller;
+import controller.member.RegisterUserController;
 import model.Member;
 import model.Task;
 import model.service.ProjectManager;
 import model.service.TaskManager;
 
 public class CreateTaskController implements Controller {
-
+	private static final Logger log = LoggerFactory.getLogger(RegisterUserController.class);
+	
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
@@ -25,23 +31,44 @@ public class CreateTaskController implements Controller {
 			System.out.println(memberList);
 			
 			request.setAttribute("memberList", memberList);
+			request.setAttribute("projectId", projectId);
+			request.setAttribute("memberList", memberList);
 			
 			return "/task/createTask.jsp";
 		}
 		
+		// POST request : 입력된 태스크 정보가 파라미터로 전달됨
+		System.out.println("task create Controller");
+		
+		int projectId = Integer.parseInt(request.getParameter("projectId"));
+		System.out.println("플젝 아디" + projectId);
+		
 		Task task = new Task();
 		
+		// 해당 태스크의 프로젝트 아이디
+		task.setProject_id(projectId);
+		
+		// JSP 폼
+		task.setName(request.getParameter("taskName"));
+		
 		int memberId = Integer.parseInt(request.getParameter("memberId"));
+		
+		// 태스크 배정, 미배정 구분
 		if (memberId != -1) {
 			task.setMember_id(memberId);
+		} else {	
+			task.setMember_id(Integer.parseInt(request.getParameter("memberId")));
 		}
+		task.setDeadline(Date.valueOf(request.getParameter("deadline")));
+		task.setContent(request.getParameter("content"));
 		
+		System.out.println(task);
+		log.debug("Create Task: {}", task);
 		
-		/*
-		 * if (tManager.insertTask(task) == 1) { System.out.println("task insert 성공"); }
-		 */
-		
-		
-		return "redirect:/project/view?step=1&&projectId="+request.getParameter("projectId");
+		if (tManager.insertTask(task) == 1) { 
+			System.out.println("task insert 성공"); 
+		}
+
+		return "redirect:/project/view?step=1&&projectId="+Integer.parseInt(request.getParameter("projectId"));
 	}
 }
